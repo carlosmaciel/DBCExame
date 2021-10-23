d33ef2416300f705bef5cbc06ceed2fba9799f14
Carlos Maciel da Silva <carlosmacie@gmail.com>
2021-10-23 09:26:13 -0300

    Comitando alterações

--- src/main/java/dbc/exception/VotacaoException.java
+++ src/main/java/dbc/exception/VotacaoException.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package dbc.exception;
 
 public class VotacaoException extends Exception {
@@ -9,15 +8,3 @@ public class VotacaoException extends Exception {
         super(message);
     }
 }
-=======
-package dbc.exception;
-
-public class VotacaoException extends Exception {
-	 
-    private static final long serialVersionUID = 4928599035264976611L;
-     
-    public VotacaoException(String message) {
-        super(message);
-    }
-}
->>>>>>> f10e9428b25df94175428da312ee99e950ea1567
