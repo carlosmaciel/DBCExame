d4210f5662a7ab14ea939ae1c67e0f9e4986d5c6
Carlos Maciel da Silva <carlosmacie@gmail.com>
2021-10-25 08:57:48 -0300

    Modificando tests

--- src/test/java/dbctests/DBCApplicationTest.java
+++ src/test/java/dbctests/DBCApplicationTest.java
@@ -1,7 +1,6 @@
 package dbctests;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
